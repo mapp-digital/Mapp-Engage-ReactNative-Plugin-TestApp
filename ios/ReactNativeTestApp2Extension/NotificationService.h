7412e6a66f67795b7ef9bcfe3525b57a6ae0255c
Stefan Stevanovic <stefan.stevanovic@mapp.com>
2025-02-07 11:10:46 +0100

    notification service added so rich push messages can be shown

---
+++ ios/ReactNativeTestApp2Extension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ReactNativeTestApp2Extension
+//
+//  Created by Stefan Stevanovic on 7.2.25..
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
