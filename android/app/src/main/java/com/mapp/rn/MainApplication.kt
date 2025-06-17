ebeb026867dd20ee355d5ffed73fbded70e6e271
tafilovic <sekitafilovic@gmail.com>
2025-06-17 18:28:23 +0200

    Updated test application with RN version 0.79.3;

--- android/app/src/main/java/com/mapp/rn/MainApplication.kt
+++ android/app/src/main/java/com/mapp/rn/MainApplication.kt
@@ -1,18 +1,16 @@
 package com.mapp.rn
 
 import android.app.Application
-import android.content.Context
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
-import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-import java.lang.reflect.InvocationTargetException
 
 class MainApplication : Application(), ReactApplication {
 
@@ -33,46 +31,14 @@ class MainApplication : Application(), ReactApplication {
         }
 
     override val reactHost: ReactHost
-        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
+        SoLoader.init(this, OpenSourceMergedSoMapping)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
             load()
-        }
-        //initializeFlipper(this, reactNativeHost.reactInstanceManager)
-    }
-
-    companion object {
-        private fun initializeFlipper(
-            context: Context, reactInstanceManager: ReactInstanceManager
-        ) {
-            if (BuildConfig.DEBUG) {
-                try {
-                    /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-                    val aClass = Class.forName("com.reactnativetestapp2.ReactNativeFlipper")
-                    aClass
-                        .getMethod(
-                            "initializeFlipper",
-                            Context::class.java,
-                            ReactInstanceManager::class.java
-                        )
-                        .invoke(null, context, reactInstanceManager)
-                } catch (e: ClassNotFoundException) {
-                    e.printStackTrace()
-                } catch (e: NoSuchMethodException) {
-                    e.printStackTrace()
-                } catch (e: IllegalAccessException) {
-                    e.printStackTrace()
-                } catch (e: InvocationTargetException) {
-                    e.printStackTrace()
-                }
-            }
         }
     }
 }
